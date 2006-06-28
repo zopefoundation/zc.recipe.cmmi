48491e543eff55e253153181577bbd401ee09552
Jim Fulton <jim@zope.com>
2006-06-28 22:10:44 +0000

    Removed dev tag. Can do this automatically, now that I know the secret trick (pass egg_info before register).

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 name = "zc.recipe.filestorage"
 setup(
     name = name,
-    version = "1.0.dev",
+    version = "1.0",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for defining a file-storage",
