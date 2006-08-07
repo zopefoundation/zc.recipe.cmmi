7050d5f46aeba892b096b2199002bc20af545e88
Jim Fulton <jim@zope.com>
2006-08-07 21:02:51 +0000

    initial mods (in progress)

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-name = "zc.recipe.filestorage"
+name = "zc.recipe.cmmi"
 setup(
     name = name,
     version = "1.0",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
-    description = "ZC Buildout recipe for defining a file-storage",
+    description = "ZC Buildout recipe for configure/make/make install",
     long_description = open('README.txt').read(),
     license = "ZPL 2.1",
     keywords = "zope3",
