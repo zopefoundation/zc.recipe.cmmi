3607b0afa357dbf98afa15a11ad04cf9f1ae1dad
Jeff Rush <xanalogica@gmail.com>
2008-11-08 03:17:20 +0000

    Made release 1.1.5.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 name = "zc.recipe.cmmi"
 setup(
     name = name,
-    version = "1.1.4",
+    version = "1.1.5",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for configure/make/make install",
