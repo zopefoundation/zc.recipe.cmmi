80a0ec9de1f335901d3eff5b7d9d689ea856f6f5
Michael Howitz <icemac@gmx.net>
2025-09-18 09:33:28 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ name = "zc.recipe.cmmi"
 
 setup(
     name=name,
-    version='5.0',
+    version='5.1.dev0',
     author="Jim Fulton",
     author_email="jim@zope.com",
     description="ZC Buildout recipe for configure/make/make install",
